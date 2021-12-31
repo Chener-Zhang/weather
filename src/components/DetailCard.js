3817b83a75a669d8008fd77ebe1e2bbdfad21595
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2021-12-30 22:18:32 -0800

    add

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function DetailCard({ cityInfo }) {
   console.log(cityInfo);
+
+  const celsius = parseFloat(cityInfo.main.temp - 273.15).toFixed(2);
+
   return (
     <>
       <h2>{cityInfo.name}</h2>
-      <h2>Temp : {cityInfo.main.temp}</h2>
+      <h2>Temp : {celsius} ℃</h2>
+      <h2>{cityInfo.weather[0].description}</h2>
+      <img
+        src={`http://openweathermap.org/img/wn/${cityInfo.weather[0].icon}@2x.png`}
+        alt={cityInfo.id}
+      />
     </>
   );
 }
