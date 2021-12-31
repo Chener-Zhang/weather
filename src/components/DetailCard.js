57bd7d4cac2977912fbf232012eeabc85c383e3c
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2021-12-30 16:26:11 -0800

    done

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 function DetailCard({ cityInfo }) {
-  console.log(cityInfo)
+  console.log(cityInfo);
   return (
     <>
       <h2>{cityInfo.name}</h2>
       <h2>Temp : {cityInfo.main.temp}</h2>
-      <h2>feels_like :  </h2>
- 
     </>
   );
 }
